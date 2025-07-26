5505513b9bc82769835059423ab9b78f0c9e7c70
Pranav Shinde <Pranav.shinde@vectorconsulting.in>
2025-07-26 23:46:26 +0530

    Correct guess, High guess and low guess cases

--- script.js
+++ script.js
@@ -14,6 +14,12 @@
 
 //NOTE: Event listener
 
+// Math.random() generates a random number between 0 (inclusive) and 1 (exclusive).
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber
+
+
+let score = 20;
 
 // We select the button with the class 'check' and add an click event listener to it.The function inside the event listener is the event handler that will be executed when the button is clicked.
 
@@ -21,9 +27,35 @@
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
 
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    // document.querySelector('.message').textContent = '🎉 Correct Number!';
 
     if(!guess){
         document.querySelector('.message').textContent = '⛔ No Number!';
+    }else if(guess === secretNumber){
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
+        
+    }else if(guess > secretNumber){
+        if(score > 1){
+        document.querySelector('.message').textContent = 'Your guess is too high!';
+
+        // Decrease score when the guess is wrong
+        score--;
+        // Display the score back to the user
+        document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.score').textContent = 0;
+            document.querySelector('.message').textContent = 'You lose!'
+        }
+    }else if(guess < secretNumber){
+        if(score > 1){
+        document.querySelector('.message').textContent = 'Your guess is too low!';
+        // Decrease score when the guess is wrong
+        score--;
+        // Display the score back to the user
+        document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.score').textContent = 0;
+            document.querySelector('.message').textContent = 'You lose!'
+        }
     }
 })
