0720cb01ab88d1337e59e86a703c486ddc9267f9
Pranav Shinde <Pranav.shinde@vectorconsulting.in>
2025-07-26 18:04:17 +0530

    value property of an input object

--- script.js
+++ script.js
@@ -3,3 +3,12 @@
 
 console.log(document.querySelector('.message'));
 console.log(document.querySelector('.message').textContent); 
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+
